57af4f1b675a0d83c6fc068941d0791b53393fed
Mavrk1604 <mavrk1604@gmail.com>
2024-08-15 21:51:11 -0500

    landing sidebar y login sidebar componentes creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LandingSidebarComponent } from './components/landing-sidebar/landing-sidebar.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: 'landing', title: `Landing`, component: LandingComponent }
