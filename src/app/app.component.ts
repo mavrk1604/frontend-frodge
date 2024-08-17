d69174320e71326d14d866acd5b47e218a8dfe6c
Mavrk1604 <mavrk1604@gmail.com>
2024-08-16 19:45:52 -0500

    componente de register creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { LandingComponent } from './components/landing/landing.component';
 import { LandingSidebarComponent } from './components/landing-sidebar/landing-sidebar.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingComponent, LoginComponent, LandingSidebarComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingComponent, LoginComponent, LandingSidebarComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LandingSidebarComponent } from './components/landing-sidebar/landing-sidebar.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: 'landing', title: `Landing`, component: LandingComponent }
