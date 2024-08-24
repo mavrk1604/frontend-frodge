898c0476a0b0ed139fde1918e8a4de4598acf920
Mavrk1604 <mavrk1604@gmail.com>
2024-08-23 21:30:30 -0500

    products component creado y maquetado, finalizado al 80%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
 
 export const routes: Routes = [
   { path: '', title: `Landing`, component: LandingComponent },
+  { path: 'products', title: 'Products', component: ProductsComponent }
 ];
