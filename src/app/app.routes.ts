07c1bc37d0ab9a2e3ef35e7ee60fdad06f549394
Mavrk1604 <mavrk1604@gmail.com>
2024-08-26 20:46:38 -0500

    componente create product modificado peor faltan ajuste css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductsComponent } from './components/products/products.component';
+import { CreateProductComponent } from './protected/create-product/create-product.component';
 
 export const routes: Routes = [
   { path: '', title: `Landing`, component: LandingComponent },
-  { path: 'products', title: 'Products', component: ProductsComponent }
+  { path: 'products', title: 'Products', component: ProductsComponent },
+  {path:'create-product', title: 'Create Product', component: CreateProductComponent}
 ];
--- src/app/protected/create-product/create-product.component.ts
+++ src/app/protected/create-product/create-product.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule, NavbarComponent],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css'
 })
