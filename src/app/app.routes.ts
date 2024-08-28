c231ae5ee5ea02cb7336abee5c261d05cd5b6bdd
Sebastian <sebastianbcrespo@gmail.com>
2024-08-28 18:34:44 -0500

    Login y register eliminados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,10 @@ import { ProductsComponent } from './components/products/products.component';
 import { CreateProductComponent } from './protected/create-product/create-product.component';
 import { CreateRecipeComponent } from './protected/create-recipe/create-recipe.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
+
 
 export const routes: Routes = [
   {path: '', title: `Landing`, component: LandingComponent },
-  {path: "register", title: "Register", component: RegisterComponent},
-  {path: "login", title: "Login", component: LoginComponent},
   { path: 'products', title: 'Products', component: ProductsComponent },
   { path:'recipes', title: 'Recipes', component:RecipesComponent},
   { path: 'create-product', title: 'Create Product', component: CreateProductComponent },
