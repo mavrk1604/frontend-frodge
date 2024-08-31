1b661fd2cf4333879a173e98224e9fe2af34ac7b
Sebastian <sebastianbcrespo@gmail.com>
2024-08-30 21:15:22 -0500

    Controlador create-recipes actualizado

--- src/app/protected/create-recipe/create-recipe.component.ts
+++ src/app/protected/create-recipe/create-recipe.component.ts
@@ -1,39 +1,56 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-recipe',
   standalone: true,
-  imports: [RouterLink, FormsModule, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, ReactiveFormsModule],
   templateUrl: './create-recipe.component.html',
   styleUrl: './create-recipe.component.css'
 })
-export class CreateRecipeComponent {
-  vegetarianSelected: boolean = false
-  recipe = {
-    name: "",
-    imageurl: "",
-    ingredients: [],
-    allergens: [],
-    preparation: "",
-    category: "",
-    vegetarian: false
-  };
 
-  constructor(private http: HttpClient) {}
+
+export class CreateRecipeComponent {
+  form!: FormGroup
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      imageurl: ['', []],
+      ingredients: ['', []],
+      allergens: ['', []],
+      preparation:  ['', []],
+      category: ['', []],
+      vegetarian: ['', []]
+    })
+  }
 
   onSubmit() {
-    this.http.post('http://localhost:8080/api/create-recipe', this.recipe)
+    console.log(this.form.value)
+    const formValues = this.form.value;
+    const ingredientsArray = this.parseIngredients(formValues.ingredients);
+
+    const recipeData = {
+      name: formValues.name,
+      imageurl: formValues.imageurl,
+      ingredients: ingredientsArray,
+      allergens: formValues.allergens,
+      preparation: formValues.preparation,
+      category: formValues.category,
+      vegetarian: formValues.vegetarian,
+    };
+
+    this.http.post('http://localhost:8080/api/create-recipe', recipeData)
       .subscribe(response => {
         console.log(response);
       });
   }
 
-  onVegetarianChange(event: Event) {
-    this.vegetarianSelected = !this.vegetarianSelected
-    this.recipe.vegetarian = this.vegetarianSelected
+  parseIngredients(ingredients: string): string[] {
+    // Split the string by commas and trim each element
+    return ingredients.split(',').map((ingredient) => ingredient.trim());
   }
 }
